ff6a92cf82c36f8c70d00d13e3de8de96f898fd7
adipurnama83 <adi.purnama1983@gmail.com>
2021-07-21 10:40:10 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,5 @@
+<?php
+
+$user_agent = "Mozilla/5.0 (Linux; Android 10; RMX1911) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.96 Mobile Safari/537.36";
+$_luid      = "1988179";
+$_lrtk      = "BVpRFLJWw80C7xgpL7KBgoUuIkWy9b8MVjISVQ3hyP9pH76s007zGpwSfYx8xtl3";
